4ba77c108fe980748616511e871f6523b3074e7c
unaguna <k.izumi.ysk@gmail.com>
2022-05-28 16:54:03 +0900

    🗃️Add table `game_status`

--- mysql/DB/tower-of-doors.sql
+++ mysql/DB/tower-of-doors.sql
@@ -117,6 +117,61 @@ INSERT INTO `azimuth_log` (`azimuth`, `timestamp`, `yawing`) VALUES
 
 
 --
+-- Table structure for table `enum_game_status`
+--
+
+DROP TABLE IF EXISTS `enum_game_status`;
+CREATE TABLE `enum_game_status` (
+  `status` char(11) NOT NULL,
+  PRIMARY KEY (`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Data for table `enum_game_status`
+--
+
+set autocommit=0;
+INSERT INTO `enum_game_status` (`status`) VALUES
+  ('MAINTENANCE'),
+  ('STANDBY'),
+  ('ON_GAME')
+;
+
+
+--
+-- Table structure for table `game_status`
+--
+
+DROP TABLE IF EXISTS `game_status`;
+CREATE TABLE `game_status` (
+  `status` char(11) NOT NULL,
+  `player_num` int unsigned CHECK (`player_num` > 0),
+  # 0: interval phase
+  # >0: player no
+  `turn_player` int unsigned,
+  `timestamp` datetime NOT NULL,
+  CONSTRAINT `player_num_must_not_be_null_when_on_game` CHECK (`status` <> 'ON_GAME' or `player_num` is not NULL),
+  CONSTRAINT `turn_player_must_not_be_null_when_on_game` CHECK (`status` <> 'ON_GAME' or `turn_player` is not NULL),
+  CONSTRAINT `player_num_must_be_null_when_not_on_game` CHECK (`status` = 'ON_GAME' or `player_num` is NULL),
+  CONSTRAINT `turn_player_must_be_null_when_not_on_game` CHECK (`status` = 'ON_GAME' or `turn_player` is NULL),
+  CONSTRAINT `turn_player_must_not_be_greater_than_player_num` CHECK (`turn_player` <= `player_num`),
+  PRIMARY KEY (`timestamp`),
+  FOREIGN KEY fk_game_status(`status`) REFERENCES `enum_game_status`(`status`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Data for table `game_status`
+--
+
+set autocommit=0;
+INSERT INTO `game_status` (`status`, `timestamp`, `player_num`, `turn_player`) VALUES
+  ('MAINTENANCE', now(), NULL, NULL)
+;
+
+
+--
 -- Table structure for table `door_log_reason`
 --
 
