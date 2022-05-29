c4a24f52c875d0056a56bfc9c6ec42ef73d6388d
unaguna <k.izumi.ysk@gmail.com>
2022-05-29 14:10:33 +0900

    ♻Add type hint

--- db-update/scripts/service/_connect.py
+++ db-update/scripts/service/_connect.py
@@ -3,7 +3,7 @@ import os
 import MySQLdb
 
 
-def connect(*, host: str = None):
+def connect(*, host: str = None) -> MySQLdb.Connection:
     if host is None:
         host = os.environ.get("MYSQL_HOST", "127.0.0.1")
 
