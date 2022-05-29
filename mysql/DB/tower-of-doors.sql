3409549f71991c3abbebace24e98990768265e77
unaguna <k.izumi.ysk@gmail.com>
2022-05-29 16:55:41 +0900

    🗃️Add field `yawing_schedule.yawing_reason`

--- mysql/DB/tower-of-doors.sql
+++ mysql/DB/tower-of-doors.sql
@@ -141,6 +141,28 @@ INSERT INTO `enum_yawing_status` (`yawing_status`) VALUES
 
 
 --
+-- Table structure for table `enum_yawing_reason`
+--
+
+DROP TABLE IF EXISTS `enum_yawing_reason`;
+CREATE TABLE `enum_yawing_reason` (
+  `yawing_reason` char(11) NOT NULL,
+  PRIMARY KEY (`yawing_reason`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+--
+-- Data for table `enum_yawing_reason`
+--
+
+set autocommit=0;
+INSERT INTO `enum_yawing_reason` (`yawing_reason`) VALUES
+  ('GAME_PHASE'),
+  ('REMOTE')
+;
+
+
+--
 -- Table structure for table `yawing_schedule`
 --
 
@@ -148,12 +170,14 @@ DROP TABLE IF EXISTS `yawing_schedule`;
 CREATE TABLE `yawing_schedule` (
   `id` BIGINT NOT NULL AUTO_INCREMENT,
   `aim_azimuth` double NOT NULL,
+  `yawing_reason` char(11) NOT NULL,
   `schedule_start_time` datetime NOT NULL,
   `schedule_end_time` datetime NOT NULL,
   `yawing_status` char(9) NOT NULL DEFAULT 'SCHEDULED',
   `actual_start_time` datetime DEFAULT NULL,
   `actual_end_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
+  FOREIGN KEY fk_yawing_reason(`yawing_reason`) REFERENCES `enum_yawing_reason`(`yawing_reason`),
   FOREIGN KEY fk_yawing_status(`yawing_status`) REFERENCES `enum_yawing_status`(`yawing_status`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
