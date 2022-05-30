f3195bbb1024214b714cb805f951f840d17a55b0
unaguna <k.izumi.ysk@gmail.com>
2022-05-30 18:29:17 +0900

    ♻Create module logic.azimuth

--- db-update/scripts/tod-bg.py
+++ db-update/scripts/tod-bg.py
@@ -3,53 +3,17 @@
 
 import asyncio
 from datetime import datetime, timedelta
-import math
-from typing import Tuple
 
 import MySQLdb
 
+import logic.azimuth
 import logic.game
-from model import YawingScheduleRecord, YawingStatus
 import service
-import service.azimuthlog
 import service.gamestatus
-import service.yawingschedule
 
 
 INTERVAL_TIME_SEC = 30
 TURN_TIME_SEC = 30
-
-
-def calc_next_azimuth(
-    yawing_schedule: YawingScheduleRecord,
-    current_azimuth: float,
-    interval: timedelta,
-    now: datetime = None,
-) -> Tuple[float, bool]:
-    if now is None:
-        now = datetime.now()
-
-    remaining_time = yawing_schedule.schedule_end_time - now
-    # Remaining yawing angle
-    # The direction of yawing is determined so that the absolute value of the yawing angle is smaller
-    remaining_azimuth = min(
-        yawing_schedule.aim_azimuth - current_azimuth,
-        yawing_schedule.aim_azimuth + 360 - current_azimuth,
-        key=abs,
-    )
-
-    # Number of azimuth change decisions to be made until the scheduled end time.
-    remaining_step = max(1, math.floor(remaining_time / interval))
-
-    # Determine the angular velocity so that the yawing is completed by the scheduled end time.
-    if remaining_step <= 1:
-        next_azimuth = yawing_schedule.aim_azimuth
-        is_last_step = True
-    else:
-        next_azimuth = (remaining_azimuth / remaining_step) + current_azimuth
-        is_last_step = False
-
-    return next_azimuth, is_last_step
 
 
 async def col_initial_connection():
@@ -100,57 +64,9 @@ async def col_azimuth_control():
 
     loop_period_time = datetime.now()
     while True:
-        with service.connect() as connection:
-            current_yawing = service.yawingschedule.get_now_yawing(
-                connection=connection
-            )
-            starting_schedule_id_list = service.yawingschedule.find_id(
-                connection=connection,
-                yawing_status=YawingStatus.SCHEDULED,
-                schedule_start_time_max=loop_period_time,
-            )
-
-            if current_yawing is not None:
-                # TODO: semi-error when len(starting_schedule_id_list) > 0
-
-                current_azimuth = service.azimuthlog.get_current_azimuth(
-                    connection=connection
-                )
-
-                # yaw (insert into azimuth_log)
-                next_azimuth, is_last_step = calc_next_azimuth(
-                    current_yawing,
-                    current_azimuth,
-                    interval=loop_interval,
-                    now=loop_period_time,
-                )
-                service.azimuthlog.insert_azimuth(
-                    next_azimuth,
-                    yawing=not is_last_step,
-                    timestamp=loop_period_time,
-                    connection=connection,
-                )
-                print("yawing: current_azimuth =", next_azimuth)
-                if is_last_step:
-                    service.yawingschedule.update_end_yawing(
-                        current_yawing,
-                        connection=connection,
-                        actual_end_time=loop_period_time,
-                    )
-                    print("complete yawing: id =", current_yawing.id)
-            else:
-                # TODO: semi-error when len(starting_schedule_id_list) >= 2
-
-                # Start yawing
-                if len(starting_schedule_id_list) > 0:
-                    yawing_schedule_id = starting_schedule_id_list[0]
-                    service.yawingschedule.update_start_yawing(
-                        yawing_schedule_id, connection=connection
-                    )
-                    # TODO: output not only id but also aim_azimuth etc.
-                    print("start yawing: id =", yawing_schedule_id)
-
-            connection.commit()
+        logic.azimuth.azimuth_control(
+            now=loop_period_time, azimuth_update_interval=loop_interval
+        )
 
         # calc waiting time for the next loop
         next_loop_period_time = loop_period_time + loop_interval
